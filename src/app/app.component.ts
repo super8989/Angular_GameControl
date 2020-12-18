5387ffd607a060fe3282a2e9ebe806a810682348
Sam <super8989@gmail.com>
2020-12-18 12:26:29 -0500

    Created components and functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'game-control';
+  evenNumbers: number[] = [];
+  oddNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    firedNumber % 2 === 0
+      ? this.evenNumbers.push(firedNumber)
+      : this.oddNumbers.push(firedNumber);
+  }
 }
